a48fe3b06cc1d13dfd9465a2f793ed433b8b4117
Pritam barik <barickpritam08@gmail.com>
2026-06-23 19:50:43 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
-   
-   </>
-  )
+    <BrowserRouter>
+      <Routes>
+       
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainHome from '../../components/MainHome/MainHome'
+import MordenIdea from '../../components/MordenIdea/MordenIdea'
+
+const Home = () => {
+  return (
+    <div>
+         <MainHome/>
+          <MordenIdea />
+    </div>
+  )
+}
+
+export default Home
